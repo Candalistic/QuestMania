4d8a9c867604c8784d5abd77cdf8bee2391fb280
Dmitry Ten <candalistic@gmail.com>
2016-07-04 13:31:42 +0300

    Entered API code for google maps. Map is not working due to exception. Needs to be resolved

--- app/src/main/java/com/mania/quest/questmania/MainActivity.java
+++ app/src/main/java/com/mania/quest/questmania/MainActivity.java
@@ -1,7 +1,9 @@
 package com.mania.quest.questmania;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onQuestsClick(View view){
+        Intent intent = new Intent(this, QuestProgressActivity.class);
+        startActivity(intent);
+    }
+
+    public void onAwardsClick(View view){
+        Intent intent = new Intent(this, AwardsActivity.class);
+        startActivity(intent);
+    }
+
+    public void onSettingsActiity(View view){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
