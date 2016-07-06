89875456e61a26298a2cb955d723813ae4c3e3a9
Dmitry Ten <candalistic@gmail.com>
2016-07-07 01:27:14 +0300

    Implemented the function to switch from the map view to the list view of the nearby quests.

--- app/src/main/java/com/mania/quest/questmania/QuestProgressActivity.java
+++ app/src/main/java/com/mania/quest/questmania/QuestProgressActivity.java
@@ -1,7 +1,13 @@
 package com.mania.quest.questmania;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,19 +15,37 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.mania.quest.questmania.dummy.DummyContent;
 
-public class QuestProgressActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
+
+public class QuestProgressActivity extends FragmentActivity implements OnMapReadyCallback,
+        ItemFragment.OnListFragmentInteractionListener{
 
     private GoogleMap mMap;
+    private SupportMapFragment mapFragment;
+    private ListView progressList;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quest_progress);
+
+        fragmentManager = getSupportFragmentManager();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        mapFragment = SupportMapFragment.newInstance();
         mapFragment.getMapAsync(this);
+        android.support.v4.app.FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, mapFragment);
+        transaction.commit();
+
+        progressList = (ListView) findViewById(R.id.quest_progress_list);
+        ArrayList<String> tempEntries = new ArrayList<>();
+        tempEntries.add("Hello World");
+        tempEntries.add("Hello World 1");
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.list_textview, tempEntries);
+        progressList.setAdapter(adapter);
     }
 
 
@@ -42,5 +66,26 @@ public class QuestProgressActivity extends FragmentActivity implements OnMapRead
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item){
+
+    }
+
+    public void onMapViewClick(View view) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, mapFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    public void onListViewClick(View view) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Fragment listFragment = new ItemFragment();
+        fragmentTransaction.replace(R.id.fragment_container, listFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
     }
 }
